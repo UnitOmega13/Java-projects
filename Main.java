50bae413beabbe1dfe9da4ababcf5a64e697aca7
Maksim Hruslov <neko08021994@gmail.com>
2019-05-28 20:14:23 +0300

    Update 1

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         int choice;
         int numberOfYears;
         int numberForBitChanger, indexOfBit;
