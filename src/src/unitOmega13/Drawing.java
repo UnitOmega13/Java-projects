7e7b1ba5729fa8cad3784ece444abfa7383604b7
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-05-30 11:26:22 +0300

    Fix, in task says that we can`t use Math.pow()

---
+++ src/src/unitOmega13/Drawing.java
@@ -0,0 +1,56 @@
+package unitOmega13;
+
+class Drawing {
+	private char[][] firstPart;
+	private int maxSizeOfStarsInLine;
+
+	void drawStars(int maxNumberOfStars) {
+		this.maxSizeOfStarsInLine = maxNumberOfStars;
+		if (maxSizeOfStarsInLine % 2 == 0) {
+			everySecondLine();
+		} else {
+			everythirdLine();
+		}
+		printStars();
+	}
+
+	private void printStars() {
+		for (int printLine = 0; printLine < maxSizeOfStarsInLine / 2 + 1; printLine++) {
+			printLines(printLine);
+		}
+		for (int lineSizetoPrint = maxSizeOfStarsInLine / 2 - 1; lineSizetoPrint >= 0; lineSizetoPrint--) {
+			printLines(lineSizetoPrint);
+		}
+	}
+
+	private void printLines(int index) {
+		for (int icell = 0; icell < maxSizeOfStarsInLine; icell++) {
+			System.out.print(firstPart[index][icell]);
+		}
+		System.out.println();
+	}
+
+	private void everySecondLine() {
+		maxSizeOfStarsInLine++;
+		everythirdLine();
+		for (int unevenLine = 1; unevenLine < maxSizeOfStarsInLine / 2 + 1; unevenLine++) {
+			firstPart[unevenLine][maxSizeOfStarsInLine / 2] = ' ';
+		}
+
+	}
+
+	private void everythirdLine() {
+		firstPart = new char[maxSizeOfStarsInLine / 2 + 1][maxSizeOfStarsInLine];
+		int newStar = maxSizeOfStarsInLine / 2;
+		int numberOfStars = 1;
+		for (int lineInCompleted = 0; lineInCompleted < maxSizeOfStarsInLine / 2 + 1; lineInCompleted++) {
+			for (int completedStars = 0; completedStars < numberOfStars; completedStars++) {
+				firstPart[lineInCompleted][newStar++] = '*';
+			}
+			if (numberOfStars + 2 <= maxSizeOfStarsInLine) {
+				numberOfStars += 2;
+				newStar = (maxSizeOfStarsInLine - numberOfStars) / 2;
+			}
+		}
+	}
+}
--- src/src/unitOmega13/MatchCalculator.java
+++ src/src/unitOmega13/MatchCalculator.java
@@ -2,17 +2,19 @@ package unitOmega13;
 
 class MatchCalculator {
 
-	void countResult(int firstTeamGoal, int secondTeamGoal, int gamerFirstResult, int gamerSecondResulr) {
-		if (firstTeamGoal < 0 || secondTeamGoal < 0 || gamerFirstResult < 0 || gamerSecondResulr < 0) {
+	void countResult(int firstTeamGoal, int secondTeamGoal, int gamerFirstResult, int gamerSecondResult) {
+		if (firstTeamGoal < 0 || secondTeamGoal < 0 || gamerFirstResult < 0 || gamerSecondResult < 0) {
 			throw new IllegalArgumentException("Number of goals should be 0 or bigger than 0");
 		}
 
 		int differenceGoals;
 		int differencePlayerGoals;
 		differenceGoals = firstTeamGoal - secondTeamGoal;
-		differencePlayerGoals = gamerFirstResult - gamerSecondResulr;
-		String resultOfGame = differenceGoals > 0 && differencePlayerGoals > 0 || differenceGoals < 0 && differencePlayerGoals < 0 ? "1" : "0";
-		resultOfGame = firstTeamGoal == gamerFirstResult && secondTeamGoal == gamerSecondResulr? "21" : "0";
+		differencePlayerGoals = gamerFirstResult - gamerSecondResult;
+		return (firstTeamGoal == gamerFirstResult && secondTeamGoal == gamerSecondResult) ? 2 :
+				(differenceGoals > 0 && differencePlayerGoals > 0 )||
+						(differenceGoals < 0 && differencePlayerGoals < 0 ) ||
+						(differenceGoals == 0 && differencePlayerGoals == 0 ) ? 1 : 0;
 		System.out.println(resultOfGame);
 	}
 }
