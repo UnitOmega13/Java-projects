7001e16895f91cac69939608b2be8ed7b62aac76
UnitOmega13 <51029392+UnitOmega13@users.noreply.github.com>
2019-05-31 18:38:32 +0300

    Delete SquareRootFinder.java

--- src/SquareRootFinder.java
+++
@@ -1,8 +0,0 @@
-public class SquareRootFinder {
-	public double sqrt(double x) {
-		if (x < 0) {
-			throw new IllegalArgumentException("Expected number, bigger than 0");
-		}
-		return Math.sqrt(x);
-	}
-}
