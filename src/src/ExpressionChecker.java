4860dcbce5a5319dec3f700585193c7d6a199d08
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-05-31 11:18:33 +0300

    CS fix

--- src/src/ExpressionChecker.java
+++ src/src/ExpressionChecker.java
@@ -1,6 +1,7 @@
 public class ExpressionChecker {
+	static final float checkFloat = 1E-4;
 
 	public boolean doubleExpression(double a, double b, double c) {
-		return Math.abs((a + b) - c) < 1E-4;
+		return Math.abs((a + b) - c) < checkFloat;
 	}
 }
