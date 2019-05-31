aec38957f6b3483f6682de3d0edb80540a3ca6ef
UnitOmega13 <51029392+UnitOmega13@users.noreply.github.com>
2019-05-31 18:09:41 +0300

    add finally

--- src/src/RobotMove.java
+++ src/src/RobotMove.java
@@ -1,14 +1,22 @@
 public class RobotMove {
-	public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
-		for (int connections = 0; connections < 3; connections++) {
-			try (RobotConnection robotConnection = robotConnectionManager.getConnection()) {
-				robotConnection.moveRobotTo(toX, toY);
-				connections = 3;
+	RobotConnection robotConnection;
+		for (int connection = 0; connection < 3; connection++) {
+			try {
+				robotConnection = robotConnectionManager.getConnection();
 			} catch (RobotConnectionException e) {
-				if (connections == 2) {
-					throw new RobotConnectionException(e.getMessage(), e);
+				continue;
+			}
+			try {
+				robotConnection.moveRobotTo(toX, toY);
+				return;
+			} catch (RobotConnectionException e) {
+			} finally {
+				try {
+					robotConnection.close();
+				} catch (RobotConnectionException e) {
 				}
 			}
 		}
+		throw new RobotConnectionException("ERROR!");
 	}
 }
