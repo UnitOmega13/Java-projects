cea5c76f347a1186986cf923c1d88ce9f224a4ea
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-05-31 23:24:35 +0300

    Change name of constant

--- src/src/ExpressionChecker.java
+++ src/src/ExpressionChecker.java
@@ -1,8 +1,8 @@
 public class ExpressionChecker {
-	private static final double checkValue = 1E-4;
+	private static final double CHECKVALUE = 1E-4;
 
 	public boolean doubleExpression(double a, double b, double c) {
 
-		return Math.abs((a + b) - c) < checkValue;
+		return Math.abs((a + b) - c) < CHECKVALUE;
 	}
 }
