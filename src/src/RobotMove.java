4666a7839ee618d37549457f5009a8981a039d93
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-06-03 14:34:03 +0300

    Fix RobotMove

--- src/src/RobotMove.java
+++ src/src/RobotMove.java
@@ -1,4 +1,11 @@
+package src;
+
+import src.RobotConnection;
+import src.RobotConnectionException;
+import src.RobotConnectionManager;
+
 public class RobotMove {
+	public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY){
 	RobotConnection robotConnection;
 		for (int connection = 0; connection < 3; connection++) {
 			try {
