017b5c82a93685dc457bc8d4f6a2abaa72b578dc
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-06-04 12:41:35 +0300

    Home work 6 for Mate academy

--- src/RobotConnection.java
+++
@@ -1,6 +0,0 @@
-public interface RobotConnection extends AutoCloseable {
-	void moveRobotTo(int x, int y);
-
-	@Override
-	void close();
-}
--- src/RobotConnectionManager.java
+++
@@ -1,3 +0,0 @@
-public interface RobotConnectionManager {
-	RobotConnection getConnection();
-}
