090f6ceec491da6230caa05af4f6eea2101a041e
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-06-11 14:56:01 +0300

    added new elements to list

--- src/Aplication.java
+++ src/Aplication.java
@@ -5,6 +5,9 @@ public class Aplication {
 		list.add("1");
 		list.add("2");
 		list.add("3");
+		list.add("5");
+		list.add("6");
+		list.add("7");
 		list.add("2+", 2);
 		System.out.println(list);
 
