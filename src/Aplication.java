40a4f2016c8195642c45b810c81821e2e1ad4c3e
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-06-14 18:15:43 +0300

    fix

--- src/Aplication.java
+++ src/Aplication.java
@@ -10,16 +10,6 @@ public class Aplication {
 		newHashMap.put(7, new Items("Triangle", "Blue", 5));
 		newHashMap.put(8, new Items("Square", "Green", 3));
 		newHashMap.put(9, new Items("Scope", "Red", 9));
-		newHashMap.put(10, new Items("Triangle", "Blue", 5));
-		newHashMap.put(11, new Items("Square", "Green", 3));
-		newHashMap.put(12, new Items("Scope", "Red", 9));
-		newHashMap.put(13, new Items("Triangle", "Blue", 5));
-		newHashMap.put(14, new Items("Square", "Green", 3));
-		newHashMap.put(15, new Items("Scope", "Red", 9));
-		newHashMap.put(16, new Items("Triangle", "Blue", 5));
-		newHashMap.put(17, new Items("Square", "Green", 3));
-		newHashMap.put(18, new Items("Scope", "Red", 9));
-		newHashMap.put(19, new Items("Triangle", "Blue", 5));
 		System.out.println(newHashMap.get(3));
 		System.out.println(newHashMap.get(2));
 		System.out.println(newHashMap.get(1));
@@ -29,16 +19,8 @@ public class Aplication {
 		System.out.println(newHashMap.get(6));
 		System.out.println(newHashMap.get(7));
 		System.out.println(newHashMap.get(8));
-		System.out.println(newHashMap.get(9));
-		System.out.println(newHashMap.get(10));
-		System.out.println(newHashMap.get(11));
-		System.out.println(newHashMap.get(12));
-		System.out.println(newHashMap.get(13));
-		System.out.println(newHashMap.get(14));
-		System.out.println(newHashMap.get(15));
-		System.out.println(newHashMap.get(16));
-		System.out.println(newHashMap.get(17));
-		System.out.println(newHashMap.get(18));
-		System.out.println(newHashMap.get(19));
+		System.out.println(newHashMap.getNode(4));
+		newHashMap.clear();
+		System.out.println(newHashMap.get(0));
 	}
 }
--- src/NewHashMap.java
+++ src/NewHashMap.java
@@ -1,58 +1,86 @@
+import java.util.NoSuchElementException;
+
 public class NewHashMap<K, V> {
 	private Node<K, V>[] table;
 	private int sizeOfTable;
 	private static final int TABLE_SIZE = 16;
 	private static final double TABLE_MAX_PERCENT_OCCUPANCY = 0.75;
 
-	public void put(K key, V value) {
-		int occupancy = (int) (table.length * TABLE_MAX_PERCENT_OCCUPANCY);
+	void put(K key, V value) {
 		if (sizeOfTable == 0) {
 			table = new Node[TABLE_SIZE];
-		} else if (occupancy <= TABLE_SIZE) {
+		} else if (table.length * TABLE_MAX_PERCENT_OCCUPANCY <= TABLE_SIZE) {
 			resizeOfTable();
 		}
+		int hashKeyIndex = hashKeyIndex(key, table.length);
 		Node<K, V> newNode = new Node<>(key, value);
-		Node<K, V> currentNode = table[hashKeyIndex(key, table.length)];
+		Node<K, V> currentNode = table[hashKeyIndex];
 		if (currentNode == null) {
-			table[hashKeyIndex(key, table.length)] = newNode;
+			table[hashKeyIndex] = newNode;
 		} else if (currentNode.keyValue.equals(key)) {
 			currentNode.itemValue = value;
 		} else {
-			while (currentNode.hasNext() && !currentNode.itemValue.equals(key)) {
+			while (currentNode.hasNext() && !currentNode.keyValue.equals(key)) {
 				currentNode = currentNode.next;
+			}
+			if (currentNode.keyValue.equals(key)) {
+				currentNode.itemValue = value;
 			}
 			currentNode.next = newNode;
 		}
 		sizeOfTable++;
 	}
 
-	public V get(K key) {
-		Node<K, V> currentNode = table[hashKeyIndex(key, table.length)];
+	V get(K key) {
+		int hashKeyIndex = hashKeyIndex(key, table.length);
+		Node<K, V> currentNode = table[hashKeyIndex];
 		if (currentNode == null) {
 			return null;
 		}
 		while (!currentNode.keyValue.equals(key) && currentNode.hasNext()) {
 			currentNode = currentNode.next;
 		}
-		return currentNode.keyValue.equals(key) ? currentNode.itemValue : null;
+		return currentNode.keyValue.equals(key) ? currentNode.getItemValue() : null;
 	}
 
-	private void resizeOfTable() {
+	private Node<K, V>[] resizeOfTable() {
+		Node<K, V> collisionNode;
 		Node[] newTable = new Node[table.length << 1];
 		for (Node<K, V> node : table) {
 			if (node != null) {
 				newTable[hashKeyIndex(node.keyValue, newTable.length)] = node;
+				if (node.hasNext()) {
+					while (node.hasNext()) {
+						collisionNode = node.next;
+						newTable[hashKeyIndex(collisionNode.keyValue, newTable.length)] = node;
+					}
+				}
 			}
 		}
+		return newTable;
 	}
 
 	public void clear() {
-		Node<K, V>[] node = table;
-		if (node != null && sizeOfTable > 0) {
-			for (int index = 0; index < node.length; index++) {
-				node = null;
-			}
+		Node<K,V>[] nodes;
+		if ((nodes = table) != null && sizeOfTable > 0) {
+			sizeOfTable = 0;
+			for (int index = 0; index < nodes.length; ++index)
+				nodes[index] = null;
 		}
+	}
+
+	Node<K, V> getNode(K key) {
+		Node<K, V> node = table[hashKeyIndex(key, table.length)];
+		if (node == null) {
+			throw new NoSuchElementException("Node with this does not exist!");
+		}
+		while (!node.getKeyValue().equals(key) && node.hasNext()) {
+			node = node.next;
+		}
+		if (!node.getKeyValue().equals(key)) {
+			throw new NoSuchElementException("Current key does not exist");
+		}
+		return node;
 	}
 
 	private int hashKeyIndex(K key, int tableLength) {
@@ -67,6 +95,14 @@ public class NewHashMap<K, V> {
 		Node(K keyValue, V itemValue) {
 			this.keyValue = keyValue;
 			this.itemValue = itemValue;
+		}
+
+		public K getKeyValue() {
+			return keyValue;
+		}
+
+		public V getItemValue() {
+			return itemValue;
 		}
 
 		boolean hasNext() {
