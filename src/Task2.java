a73992385af9f2c97f8aaf467ced0a364348f657
UnitOmega13 <51029392+UnitOmega13@users.noreply.github.com>
2019-06-19 02:05:06 +0300

    Update Task2.java

--- src/Task2.java
+++ src/Task2.java
@@ -2,7 +2,7 @@ import java.util.function.DoubleUnaryOperator;
 
 public class Task2 {
 	public static double integrate(DoubleUnaryOperator f, double a, double b) {
-		int n = 10000000;
+		int n = 1000000;
 		double dx = (b - a) * 1.0 / n;
 		double integral = 0;
 		double  x;
