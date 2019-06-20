554538396ebb602f40c2950099b72165957c5729
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-06-20 14:05:26 +0300

    fix

--- src/RandomStream.java
+++ src/RandomStream.java
@@ -1,40 +1,9 @@
 import java.util.function.IntSupplier;
 import java.util.stream.IntStream;
 
-public class RandomStream  {
+public class RandomStream {
 
 	public static IntStream pseudoRandomStream(int seed) {
-		IntSupplier generator = new IntSupplier() {
-			int current = 0;
-
-			int mid(int index) {
-				if (index == 0) {
-					return seed;
-				}
-				int midElement = mid(index - 1);
-				midElement *= midElement;
-				int result = 0;
-				int count = 0;
-				int res;
-				while (midElement > 0) {
-					res = midElement % 10;
-					count++;
-					if (count >= 2 && count <= 4) {
-						result += res * Math.pow(10, count - 2);
-					}
-					midElement /= 10;
-				}
-
-				return result;
-			}
-
-			public int getAsInt() {
-				return mid(current++);
-			}
-		};
-
-		IntStream natural = IntStream.generate(generator);
-
-		return natural;
+		return IntStream.iterate(seed, number -> number * number / 10 % 1000);
 	}
 }
