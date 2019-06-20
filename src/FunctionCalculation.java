99131b25a88983a8f2023bcd8a37bca6b7fdb917
UnitOmega13 <51029392+UnitOmega13@users.noreply.github.com>
2019-06-20 13:15:37 +0300

    Update FunctionCalculation.java

--- src/FunctionCalculation.java
+++ src/FunctionCalculation.java
@@ -3,12 +3,11 @@ import java.util.function.DoubleUnaryOperator;
 public class FunctionCalculation {
 	public static double integrate(DoubleUnaryOperator f, double a, double b) {
 		int n = 10_000_000;
-		double differential = (b - a) * 1.0 / n;
+		double differential = (b - a) / n;
 		double integral = 0;
-		double  x;
 		for(int number = 0; number < n ; number++){
-			x = a + number * differential;
-			integral += f.applyAsDouble(x)*differential;
+			double x = a + number * differential;
+			integral += f.applyAsDouble(x) * differential;
 		}
 		return integral;
 	}
