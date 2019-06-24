3de518a4079504ee933e8756a579e842b66f0355
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-06-24 13:53:56 +0300

    Massive change

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,22 @@
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		StringBuilder builder = new StringBuilder();
 		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNextLine()) {
-			builder.append(scanner.nextLine());
-			builder.append(" ");
-		}
-		String text = builder.toString().replaceAll("[^a-zA-Zа-яА-Я0-9]", " ");
-		Map<String, Integer> wordsByNumber = new HashMap<>();
-		scanner = new Scanner(text);
-		while (scanner.hasNext()) {
-			String word = scanner.next().toLowerCase();
-			if (wordsByNumber.containsKey(word)) {
-				wordsByNumber.put(word, wordsByNumber.get(word) + 1);
-			} else {
-				wordsByNumber.put(word, 1);
-			}
-		}
-		Map<Integer, List<String>> numberByWords = new HashMap<>();
-		for (Map.Entry<String, Integer> entry : wordsByNumber.entrySet()) {
-			String word = entry.getKey();
-			Integer number = entry.getValue();
-			if (!numberByWords.containsKey(number)) {
-				numberByWords.put(number, new ArrayList<>());
-			}
-			numberByWords.get(number).add(word);
-		}
-		List<String> allWords = new ArrayList<>();
-		numberByWords.keySet().stream()
-				.sorted(Comparator.reverseOrder())
-				.forEach(key -> {
-					List<String> words = numberByWords.get(key);
-					words.stream().sorted().forEach(value -> {
-						if (allWords.size() >= 10) {
-							return;
-						}
-						allWords.add(value);
-					});
-				});
-		allWords.forEach(System.out::println);
+		String text = scanner.nextLine();
+		Stream.of(text.toLowerCase().split("[\\p{Punct}\\s]+"))
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+				.entrySet()
+				.stream()
+				.sorted(Map.Entry.<String, Long>comparingByValue()
+						.reversed()
+						.thenComparing(Map.Entry.comparingByKey()))
+				.limit(10)
+				.map(Map.Entry::getKey)
+				.forEachOrdered(System.out::println);
 	}
 }
