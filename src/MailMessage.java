63e12ac621cbc75033a3198c6feb6af0c15e8e8e
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-06-26 22:49:41 +0300

    Fix

--- src/MailMessage.java
+++ src/MailMessage.java
@@ -2,18 +2,22 @@ public class MailMessage implements Sendable<String> {
 	private String sFrom;
 	private String sTo;
 	private String sContent;
-	public String getFrom(){
+
+	public String getFrom() {
 		return sFrom;
 	}
+
 	@Override
-	public String getTo(){
+	public String getTo() {
 		return sTo;
 	}
+
 	@Override
-	public String getContent(){
+	public String getContent() {
 		return sContent;
 	}
-	public MailMessage (String From, String To, String Content){
+
+	public MailMessage(String From, String To, String Content) {
 		sFrom = From;
 		sTo = To;
 		sContent = Content;
--- src/Sendable.java
+++ src/Sendable.java
@@ -1,4 +1,5 @@
 public interface Sendable<T> {
 	public String getTo();
+
 	public T getContent();
 }
