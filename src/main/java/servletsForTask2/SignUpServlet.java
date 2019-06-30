d9faa94e0088c76cf43e315e17fe47451c038fef
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-06-30 22:10:33 +0300

    HomeWork 16 for Mate Academy

---
+++ src/main/java/servletsForTask2/SignUpServlet.java
@@ -0,0 +1,29 @@
+package servletsForTask2;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignUpServlet extends HttpServlet {
+	private final AccountService accountService;
+	public SignUpServlet(AccountService accountService) {
+		super();
+		this.accountService = accountService;
+	}
+	@Override
+	protected void doPost(HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
+		String login = servletRequest.getParameter("login");
+		String password = servletRequest.getParameter("password");
+		servletResponse.setContentType("text/html;charset=utf-8");
+		if (login != null && password != null) {
+			accountService.addNewUser(new UserProfile(login, password, "unknown"));
+			servletResponse.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			servletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		}
+
+	}
+}
