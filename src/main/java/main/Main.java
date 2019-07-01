3ff2152a1034278df764738b041d55303ceb5c81
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-07-01 18:25:55 +0300

    HomeWork 18 for Mate Academy

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,55 +1,83 @@
 package main;
 
-import accounts.AccountService;
-import accounts.UserProfile;
-import org.eclipse.jetty.server.Handler;
+
+import dbService.DBException;
+import dbService.DBService;
+import dbService.dataSets.UsersDataSet;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.SessionsServlet;
-import servlets.UsersServlet;
-import servletsForTask2.SignInServlet;
-import servletsForTask2.SignUpServlet;
+import org.eclipse.jetty.servlet.ServletHandler;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
 
 /**
  * @author v.chibrikov
- * <p>
- * Пример кода для курса на https://stepic.org/
- * <p>
- * Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
  */
 public class Main {
-	public static void main(String[] args) throws Exception {
-		AccountService accountService = new AccountService();
-//		For Task 1
-//		AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
-//		MirrorServlet mirrorServlet = new MirrorServlet();
+    public static void main(String[] args) throws Exception {
 
-		accountService.addNewUser(new UserProfile("admin"));
-		accountService.addNewUser(new UserProfile("test"));
+        //exampleOfLesson();
+        startServer();
 
-		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-		context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
-		context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
-		context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
-		context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
-//		For Task 1
-//		context.addServlet(new ServletHolder(allRequestsServlet), "/*");
-//		context.addServlet(new ServletHolder(mirrorServlet), "/mirror");
+    }
 
-		ResourceHandler resource_handler = new ResourceHandler();
-		resource_handler.setResourceBase("public_html");
+    private static void exampleOfLesson() {
+        DBService dbService = new DBService();
+        dbService.printConnectInfo();
 
-		HandlerList handlers = new HandlerList();
-		handlers.setHandlers(new Handler[]{resource_handler, context});
+        try {
+            long userId = dbService.addUser("tully2");
+            System.out.println("Added user id: " + userId);
 
-		Server server = new Server(8080);
-		server.setHandler(handlers);
+            UsersDataSet dataSet = dbService.getUser(userId);
+            System.out.println("User data set: " + dataSet);
 
-		server.start();
-		System.out.println("Server started");
-		server.join();
-	}
+        } catch (DBException e) {
+            e.printStackTrace();
+        }
+
+        dbService.close();
+
+        // можно и так, закрытие в finalize()
+//        dbService = null;
+//        System.gc();
+    }
+
+    private static void startServer() throws Exception {
+        // https://www.eclipse.org/jetty/documentation/9.3.x/embedding-jetty.html
+
+        // Create a basic jetty server object that will listen on port 8080.
+        // Note that if you set this to port 0 then a randomly available port
+        // will be assigned that you can either look in the logs for the port,
+        // or programmatically obtain it for use in test cases.
+        Server server = new Server(8080);
+
+        // The ServletHandler is a dead simple way to create a context handler
+        // that is backed by an instance of a Servlet.
+        // This handler then needs to be registered with the Server object.
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+
+        // Passing in the class for the Servlet allows jetty to instantiate an
+        // instance of that Servlet and mount it on a given context path.
+
+        // IMPORTANT:
+        // This is a raw Servlet, not a Servlet that has been configured
+        // through a web.xml @WebServlet annotation, or anything similar.
+        handler.addServletWithMapping(SignUpServlet.class, "/signup");
+        handler.addServletWithMapping(SignInServlet.class, "/signin");
+
+        // Start things up!
+        server.start();
+        System.out.println("Server started");
+
+        // The use of server.join() the will make the current thread join and
+        // wait until the server is done executing.
+        // See
+        // http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
+        server.join();
+    }
 }
