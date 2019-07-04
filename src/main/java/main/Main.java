6f9c2c5e09844841d2e2982328c9103c1c2b22b2
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-07-04 15:40:01 +0300

    HomeWork 18 for Mate Academy

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,12 +2,15 @@ package main;
 
 
 import dbService.DBException;
-import dbService.DBService;
 import dbService.dataSets.UsersDataSet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 import servlets.SignInServlet;
 import servlets.SignUpServlet;
+
+import java.dbService.DataBaseException;
+import java.dbService.DataBaseService;
+import java.dbService.dataSets.UsersDataSet;
 
 /**
  * @author v.chibrikov
@@ -18,11 +21,14 @@ import servlets.SignUpServlet;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        //exampleOfLesson();
         startServer();
+
     }
 
     private static void exampleOfLesson() {
-        DBService dbService = new DBService();
+        DataBaseService dbService = new DataBaseService();
         dbService.printConnectInfo();
 
         try {
@@ -32,22 +38,49 @@ public class Main {
             UsersDataSet dataSet = dbService.getUser(userId);
             System.out.println("User data set: " + dataSet);
 
-        } catch (DBException e) {
+        } catch (DataBaseException e) {
             e.printStackTrace();
         }
 
         dbService.close();
+
+        // можно и так, закрытие в finalize()
+//        dbService = null;
+//        System.gc();
     }
 
     private static void startServer() throws Exception {
+        // https://www.eclipse.org/jetty/documentation/9.3.x/embedding-jetty.html
 
+        // Create a basic jetty server object that will listen on port 8080.
+        // Note that if you set this to port 0 then a randomly available port
+        // will be assigned that you can either look in the logs for the port,
+        // or programmatically obtain it for use in test cases.
         Server server = new Server(8080);
+
+        // The ServletHandler is a dead simple way to create a context handler
+        // that is backed by an instance of a Servlet.
+        // This handler then needs to be registered with the Server object.
         ServletHandler handler = new ServletHandler();
         server.setHandler(handler);
+
+        // Passing in the class for the Servlet allows jetty to instantiate an
+        // instance of that Servlet and mount it on a given context path.
+
+        // IMPORTANT:
+        // This is a raw Servlet, not a Servlet that has been configured
+        // through a web.xml @WebServlet annotation, or anything similar.
         handler.addServletWithMapping(SignUpServlet.class, "/signup");
         handler.addServletWithMapping(SignInServlet.class, "/signin");
+
+        // Start things up!
         server.start();
         System.out.println("Server started");
+
+        // The use of server.join() the will make the current thread join and
+        // wait until the server is done executing.
+        // See
+        // http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
         server.join();
     }
 }
