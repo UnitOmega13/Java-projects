91f3c7341d05561ea5ebb51bfb95afb1979c7ec4
Maksim Hruslov <fallenghostirbis@gmail.com>
2019-08-19 16:06:24 +0300

    Servlet example

--- src/main/src/main/java/controller/SignInServlet.java
+++ src/main/src/main/java/controller/SignInServlet.java
@@ -3,7 +3,6 @@ package controller;
 import service.AccountService;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
